00727093ae0bfe31b0dc2f842860f5cbf73a316f
FELIPE BLAUSTEIN <47807182@ort>
2023-04-11 09:48:22 -0300

    se agrego clase boxeador

---
+++ boxeador.cs
@@ -0,0 +1,25 @@
+﻿class boxeador
+{
+    public string nombre{get; set;}
+    public string pais{get; set;}
+    public int peso{get; set;}
+    public int potenciaGolpes{get; set;}
+    public int velocidadPiernas{get; set;}
+    public boxeador(string nom, string pa, int pe, int pg, int vp)
+    {
+        nombre = nom;
+        pais = pa;
+        peso = pe;
+        potenciaGolpes = pg;
+        velocidadPiernas = vp;
+    }
+    public double obtenerSkills()
+    {
+        double devolver = 0;
+        Random r= new Random ();
+        devolver += potenciaGolpes * 0.8;
+        devolver += velocidadPiernas * 0.6;
+        devolver += r.Next(11);
+        return devolver;
+    }
+}
