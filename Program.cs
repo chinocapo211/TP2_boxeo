24df0cefb83cc06f3a0c5bcde64cb9aefdf19548
FELIPE BLAUSTEIN <47807182@ort>
2023-04-11 11:24:24 -0300

    Se termino el trabajo

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 boxeador peleador1 =  new boxeador();
 boxeador peleador2 =  new boxeador();
 do{
-    numMenu = pedirInt(1,4,"Ingrese la opcion del menu \n1)Cargar Boxeador 1 \n2)Cargar Boxeador 2 \n3)Pelea \n4)Salir");
+    numMenu = pedirInt(1,6,"Ingrese la opcion del menu \n1)Cargar Boxeador 1 \n2)Cargar Boxeador 2 \n3)Pelea \n4)Salir \n5)Cambiar la potencia de los golpes y velocidad de las piernas de un peleador \n6)Potencia y velocidad aleatoria");
     switch(numMenu)
     {
         case 1:
@@ -16,24 +16,43 @@ do{
             {
                 double puntaje = peleador1.obtenerSkills() -  peleador2.obtenerSkills();
                 string nombreGanador = "";
-
                 if (puntaje > 0) nombreGanador=peleador1.nombre;
                 else if (puntaje < 0) 
                 {
                     nombreGanador = peleador2.nombre;
                     puntaje *= -1;
                 }
-
                 if (puntaje == 0) Console.WriteLine("Empate");
                 else if (puntaje > 30) Console.WriteLine($"Ganó {nombreGanador} por KO");
                 else if (puntaje > 10 && puntaje <= 30) Console.WriteLine($"Ganó {nombreGanador} por puntos en fallo unánime");
                 else if (puntaje > 0 && puntaje <= 10) Console.WriteLine($"Ganó {nombreGanador} por puntos en fallo dividido");
-
             }
             else
             {
                 Console.WriteLine("Alguno de los dos boxeadores no esta cargado!");
             }
+        break;
+        case 5:
+            string buscar =  pedirCadena("Ingrese el nombre del peleador al cual le quiere cambiar los atributos");
+            if(peleador1.nombre == buscar)
+            {
+                peleador1.potenciaGolpes = pedirInt(1,100,"Ingrese la nueva potencia de los golpes del peleador");
+                peleador1.velocidadPiernas = pedirInt(1,100,"Ingrese la nueva velocidad de las piernas del peleador");
+            }
+            else if(peleador2.nombre == buscar)
+            {
+                peleador2.potenciaGolpes = pedirInt(1,100,"Ingrese la nueva potencia de los golpes del peleador");
+                peleador2.velocidadPiernas = pedirInt(1,100,"Ingrese la nueva velocidad de las piernas del peleador");
+            }
+            else
+            {
+                Console.WriteLine("No se a encontrado un peleador con ese nombre");
+            }
+        break;
+        case 6:
+            peleador1.randomSkill();
+            peleador2.randomSkill();
+            Console.WriteLine("Se hicieron aleatorios la potencia y velocidad");
         break;
     }
 }while(numMenu != 4);
@@ -41,13 +60,14 @@ do{
 boxeador crearPeleador()
 {
     string nombre,pais;
-    int peso,potenciaGolpes,velocidadPiernas;
+    int peso,potenciaGolpes,velocidadPiernas,inteligencia;
     nombre = pedirCadena("Ingrese el nombre del peleador");
     pais = pedirCadena("Ingrese el pais del peleador");
     peso = pedirInt(1,150,"Ingrese el peso del peleador");
     potenciaGolpes = pedirInt(1,100,"Ingrese la potencia de los golpes del peleador");
     velocidadPiernas = pedirInt(1,100,"Ingrese la velocidad de piernas del peleador");
-    boxeador devolver = new boxeador(nombre,pais,peso,potenciaGolpes,velocidadPiernas);
+    inteligencia = pedirInt(1,100,"Ingrese la inteligencia del boxeador");
+    boxeador devolver = new boxeador(nombre,pais,peso,potenciaGolpes,velocidadPiernas,inteligencia);
     return devolver;
 }
 string pedirCadena(string msj)
--- boxeador.cs
+++ boxeador.cs
@@ -1,28 +1,37 @@
 ﻿class boxeador
 {
+    Random r= new Random ();
     public string nombre{get; set;}
     public string pais{get; set;}
     public int peso{get; set;}
     public int potenciaGolpes{get; set;}
     public int velocidadPiernas{get; set;}
+    public int inteligencia {get; set;}
     public boxeador()
     {
     }
-    public boxeador(string nom, string pa, int pe, int pg, int vp)
+    public boxeador(string nom, string pa, int pe, int pg, int vp, int intel)
     {
         nombre = nom;
         pais = pa;
         peso = pe;
         potenciaGolpes = pg;
         velocidadPiernas = vp;
+        inteligencia = intel;
     }
     public double obtenerSkills()
     {
         double devolver = 0;
-        Random r= new Random ();
+        
         devolver += potenciaGolpes * 0.8;
         devolver += velocidadPiernas * 0.6;
         devolver += r.Next(11);
+        devolver += inteligencia * 0.25;
         return devolver;
+    }
+    public void randomSkill()
+    {
+        potenciaGolpes = r.Next(101);
+        velocidadPiernas = r.Next(101);
     }
 }
